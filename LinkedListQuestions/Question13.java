8b39045154b2b2057c9b0cb124ef21a322e72ad7
somnath <somnathmishra100dbi@gmail.com>
2023-11-11 00:04:33 +0530

    updates

--- LinkedListQuestions/Question13.java
+++ LinkedListQuestions/Question13.java
@@ -1,7 +1,31 @@
 //REVERSE LINKEDLIST  USING WITHOUT RECURSSION
 
+// code order is important
+
 package LinkedListQuestions;
 
+import LinkedListQuestions.Question2.ListNode;
+
 public class Question13 {
+
+     public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode after = null;
+
+        while(curr != null){
+            after = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr= after;
+
+
+        }
+
+        // curr -> null
+
+        return prev;
+        
+    }
     
 }
