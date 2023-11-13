5e70f1419ae6cfa929b5c1a32acf86b4b89a9b64
somnath <somnathmishra100dbi@gmail.com>
2023-11-13 16:47:50 +0530

    updates

---
+++ LinkedListQuestions/Question18.java
@@ -0,0 +1,109 @@
+//REMOVE DUPLICATES 
+
+package LinkedListQuestions;
+
+public class Question18 {
+
+    static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void display(Node head) {
+
+        if (head == null)
+            return;
+
+        System.out.print(head.val + " ");
+        display(head.next);
+
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(1);
+        Node c = new Node(1);
+        Node d = new Node(2);
+        Node e = new Node(2);
+        Node f = new Node(4);
+
+        Node g = new Node(4);
+
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        f.next = g;
+        g.next = null;
+
+        display(a);
+        System.out.println();
+        
+        Node dummy = new Node(500);
+        dummy.next = a;
+        Node prev = dummy;
+        Node curr = a;
+        while(curr != null && curr.next != null){
+            while (curr != null &&  curr.next != null && prev.next.val == curr.next.val) {
+                curr = curr.next;
+                
+            }
+
+            if(prev.next == curr){
+                prev = prev.next;
+            }
+            else{
+                prev.next = curr.next;
+                curr = curr.next;
+                // prev = curr;
+
+            }
+
+        }
+
+            
+
+        display(dummy.next);
+
+        Node dummy2 = new Node(-1);
+        Node t1 = dummy2;
+        Node t2 = a;
+        while(t2 != null){
+            // if(t1.val == t2.val){
+            //     t2 = t2.next;
+            // }
+            // else{
+            //     t1.next = t2;
+            //     t2 = t2.next;
+            //     t1 = t1.next;
+            // }
+
+            if(t1.val != t2.val){
+                if(t1.next == t2){
+                    t1 = t1.next;
+                    // t2 = t2.next;
+                }
+                else{
+                    t1.next = t2;
+
+                }
+            }else{
+                t2 = t2.next;
+            }
+        }
+
+        t1.next = null;
+
+        System.out.println();
+
+        display(dummy2.next);
+
+    }
+
+}
---
+++ LinkedListQuestions/Question19.java
@@ -0,0 +1,104 @@
+//REMOVE DUPLICATES IN UNSORTED LINKED LIST
+package LinkedListQuestions;
+
+import java.util.HashSet;
+
+
+public class Question19 {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static void display(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    public static Node removeDup(Node head){
+        // Node p1 = head;
+        // Node p2 = p1;
+        // while(p1 != null && p1.next != null){
+        //     while(p2 != null && p2.next != null){
+        //         if(p2.next.data == p1.data){
+        //             p2.next = p2.next.next;
+        //         }
+        //         else{
+        //             p2 = p2.next;
+        //         }
+        //     }
+        //     p1 = p1.next;
+        //     p2 = p1;
+        // }
+
+        // return head;
+
+
+        
+            // if (head == null) {
+            //     return null;
+            // }
+        
+
+            // more optimized code
+            Node current = head;
+            Node previous = null;
+            HashSet<Integer> uniqueValues = new HashSet<>();
+        
+            while (current != null) {
+                if (uniqueValues.contains(current.data)) {
+                    // Duplicate found, remove the current node
+                    previous.next = current.next;
+                } else {
+                    // Add the current value to the set
+                    uniqueValues.add(current.data);
+                    previous = current;
+                }
+        
+                current = current.next;
+            }
+        
+            return head;
+        
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(12);
+        Node c = new Node(11);
+        Node d = new Node(11);
+        Node e = new Node(12);
+        Node f = new Node(11);
+
+        Node g = new Node(10);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        f.next = g;
+        g.next = null;
+        System.out.println("Linked List before removing duplicates : ");
+                
+
+        display(a);
+        System.out.println();
+        removeDup(a);
+        System.out.println("Linked List after removing duplicates : ");
+        display(a);
+        
+
+                
+
+    }
+}
